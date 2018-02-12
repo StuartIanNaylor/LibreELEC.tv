0b4f63c09425c6c6b0113337777643f74db805d1
cvh <namerp@googlemail.com>
2018-02-12 14:47:45 +0100

    jj

--- packages/debug/valgrind/package.mk
+++ packages/debug/valgrind/package.mk
@@ -17,16 +17,18 @@
 ################################################################################
 
 PKG_NAME="valgrind"
-PKG_VERSION="3.13.0"
-PKG_SHA256="d76680ef03f00cd5e970bbdcd4e57fb1f6df7d2e2c071635ef2be74790190c3b"
+PKG_VERSION="dcb83cf"
+#PKG_SHA256="d76680ef03f00cd5e970bbdcd4e57fb1f6df7d2e2c071635ef2be74790190c3b"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://valgrind.org/"
-PKG_URL="http://sourceware.org/pub/valgrind/$PKG_NAME-$PKG_VERSION.tar.bz2"
+#PKG_URL="http://sourceware.org/pub/valgrind/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="http://repo.or.cz/$PKG_NAME.git/snapshot/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="debug"
 PKG_SHORTDESC="A tool to help find memory-management problems in programs"
 PKG_LONGDESC="A tool to help find memory-management problems in programs"
+PKG_TOOLCHAIN="autotools"
 
 if [ "$TARGET_ARCH" = "arm" ]; then
   PKG_CONFIGURE_OPTS_TARGET="--enable-only32bit"
