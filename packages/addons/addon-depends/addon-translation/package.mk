5ff192c3467faa4ed6dd29deeb052f4b0e15c84f
cvh <namerp@googlemail.com>
2018-03-06 18:45:51 +0100

    translation

---
+++ packages/addons/addon-depends/addon-translation/package.mk
@@ -0,0 +1,29 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="addon-translation"
+PKG_VERSION="9d97b5c"
+PKG_SHA256="30925bfc38d36eeda79b8f8ff2d4813b91defd67e4861aafbd277ab75a9a8c90"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://translate.libreelec.tv"
+PKG_URL="https://github.com/CvH/translation/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="translation-${PKG_VERSION}*"
+PKG_SHORTDESC="Translations for all Add-ons."
+PKG_LONGDESC="Source of all translations for the addons."
+PKG_TOOLCHAIN="manual"
