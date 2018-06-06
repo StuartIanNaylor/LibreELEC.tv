3dda97e26507aed71308d12c77b5cc6a17dccd39
CvH <namerp@googlemail.com>
2018-06-06 11:35:30 +0200

    libXi: add +pic

--- packages/x11/lib/libXi/package.mk
+++ packages/x11/lib/libXi/package.mk
@@ -25,8 +25,8 @@ PKG_SITE="http://www.x.org/"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/lib/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain util-macros libX11 inputproto libXfixes"
 PKG_SECTION="x11/lib"
-PKG_SHORTDESC="libxi: X11 Input extension library"
 PKG_LONGDESC="LibXi provides an X Window System client interface to the XINPUT extension to the X protocol."
+PKG_BUILD_FLAGS="+pic"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared \
                            --enable-malloc0returnsnull \
