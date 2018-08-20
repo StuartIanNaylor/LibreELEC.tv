6002026ef6f4cd6720a2e9821e858931e59e7a2b
chewitt <github@chrishewitt.net>
2018-08-20 20:51:41 +0100

    linux: update amlogic-mainline to 4.18.5

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -34,6 +34,14 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
     PKG_BUILD_PERF="no"
     ;;
+  amlogic-mainline)
+    PKG_VERSION="96158f3a9e7027056a2e35cff5212434294b3b34" # 4.18.5
+    PKG_SHA256="dda226d2ab6720415d1156f62bd7cc33139923cdf846c417b821f484aade4473"
+    PKG_URL="https://github.com/torvalds/linux/archive/$PKG_VERSION.tar.gz"
+    PKG_SOURCE_DIR="$PKG_NAME-$PKG_VERSION*"
+    PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET u-boot-tools-aml:host"
+    PKG_PATCH_DIRS="default"
+    ;;
   rockchip-4.4)
     PKG_VERSION="bca2464422eb8dd734f9218265dae256a82299be"
     PKG_SHA256="baaea04ca4a1b34e0bfce36bfcf74d65b06ae371e29fa2ef96d26327e55b690d"
