7c59e367e671bc682dd528e503a694567d6460c8
CvH <namerp@googlemail.com>
2018-09-03 21:47:58 +0200

    libprojectM: update to 8f442c5

--- packages/graphics/libprojectM/package.mk
+++ packages/graphics/libprojectM/package.mk
@@ -1,16 +1,24 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libprojectM"
-PKG_VERSION="2.0.0"
-PKG_SHA256="77ed43508ae2913261714b85364198f250af9e53d36d637320ddbcc2578148ee"
+PKG_VERSION="8f442c5b17af63e3584bfcd0e537b7814b599677"
+PKG_SHA256="967634b830bc0025ef724a9ea4dc2ba4394f5a4d51484433b6d64fa0ee3e1dd0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="http://projectm.sourceforge.net/"
-PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain ftgl freetype $OPENGL"
-PKG_SECTION="multimedia"
-PKG_SHORTDESC="a MilkDrop compatible opensource music visualizer"
+PKG_SITE="https://github.com/projectM-visualizer/projectm"
+PKG_URL="https://github.com/projectM-visualizer/projectm/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="projectm-${PKG_VERSION}*"
+PKG_DEPENDS_TARGET="toolchain freetype glm $OPENGL"
 PKG_LONGDESC="a MilkDrop compatible opensource music visualizer"
+PKG_TOOLCHAIN="configure"
+PKG_BUILD_FLAGS="+pic"
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_PROJECTM_STATIC=1"
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
+                           --enable-static"
+
+# workaround due broken release files, remove at next bump
+pre_configure_target() {
+  ./autogen.sh
+}
