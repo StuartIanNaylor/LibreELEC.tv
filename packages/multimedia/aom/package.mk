96d000c12ca9f17223ca9f0fd81029330c75f3cc
CvH <namerp@googlemail.com>
2018-09-05 14:01:09 +0200

    aom: update to e1cbb13

--- packages/multimedia/aom/package.mk
+++ packages/multimedia/aom/package.mk
@@ -2,14 +2,13 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="aom"
-PKG_VERSION="96ee0eb"
-PKG_SHA256="37c8d930cc105ccad4987c65751400fc42819b77fd487c9ef19cadee0c95a2d8"
+PKG_VERSION="e1cbb13df2493a30778dae04e0728a1b45f892c7"
+PKG_SHA256="b6710a9c5c1d7d46adf2c34c4188c87aac15d71c49d06b6351adc2e3addd51dd"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
 PKG_SITE="https://www.webmproject.org"
 PKG_URL="http://repo.or.cz/aom.git/snapshot/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="multimedia"
 PKG_LONGDESC="AV1 Codec Library"
 
 PKG_CMAKE_OPTS_TARGET="-DENABLE_CCACHE=1 \
